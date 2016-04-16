a095397926676c51844c91f185631ff752a51491
Darcks OMega <Darcks OMega@DarcksOMega-PC>
2016-04-16 18:26:50 -0500

    Imprimir lista

---
+++ data_structures_recuperation/src/BuscarImagen/ImageFile.java
@@ -0,0 +1,18 @@
+package BuscarImagen;
+
+/**
+ *
+ * @author Darcks OMega
+ */
+public class ImageFile {
+    
+    protected String name;
+    protected ImageFile next, back;
+    
+    public ImageFile(String nmae){
+        this.name = name;
+        next = null;
+        back = null;
+    }
+    
+}
